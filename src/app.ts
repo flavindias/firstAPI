3854fad13d94cd4e837e2fe4aa67c81ce4875d67
Flaviano Dias <flavindias@msn.com>
2021-04-30 15:31:46 -0300

    ✨ adding dockerfile conf

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express from 'express';
-
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const {
+  PORT,
+} = process.env;
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+const port = `${PORT}`;
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "My First Express API"
+  });
 });
 app.listen(port, err => {
   if (err) {
